6f65d3e143abe23c79c08bf3f8b5e0ca917f500b
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-02-25 14:03:19 -0400

    primer comit de este proyecto

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <Nav/>
+  <div class="container">
+    <Hero/>
+    <div class="margen-idk">
+      <br>
+    </div>
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+    <SmallCardDisplay
+    v-for="cardInfo in smallCardInfo"
+    :key="cardInfo.id"
+    :cardsSection="cardInfo"
+    />
+
+  </div>
+</div>
+</template>
+
+<script>
+import { largeCardSections } from "../store/data";
+import { smallCardSections } from "../store/data";
+export default {
+  name: 'IndexPage',
+  data(){
+    return{
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .margen-idk{
+    margin-top: 10rem;
+    height: 1px;
+  }
+</style>>
+
